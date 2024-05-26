bf721cbcbe985b863e8f9c3e8018933d10ef5fde
Nicolas <axel.maldonado@est.fi.uncoma.edu.ar>
2024-05-26 15:35:57 -0300

    Ejercicios 1 y 2, controladores incompletos

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Login usuario</h1>
+        </header>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+/** return view('welcome') devolvería el archivo welcome.blade.php
+ *  La ruta de la línea Route::get es la direccion de la URL.
+ *  return view() es el archivo a retornar dentro de la carpeta view.
+ *  Se pueden ver las rutas creadas con 'php artisan route:list' en la consola.
+ *  
+*/
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+
+Route::get('/category', function () {
+    return view('category.index');
+});
+
+Route::get('/category/show/{id}', function () {
+    return view('category.show');
+});
+
+Route::get('/category/create', function () {
+    return view('category.create');
+});
+
+Route::get('/category/edit/{id}', function () {
+    return view('category.edit');
+});
+
+//Route::get('/user', 'App\Http\Controllers\UserController@__invoke')-> name('user');
