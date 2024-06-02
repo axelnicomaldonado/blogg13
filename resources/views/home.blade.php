5c801198e88c974f77163f5519d59992d66a729b
elpista <espadachin2003@gmail.com>
2024-06-02 20:20:29 -0300

    home empezado y añadido a las routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,36 @@
 
     <body>
         <header>
-            <h1>Pantalla principal</h1>
+            <h1>Blogg13</h1>
         </header>
+        <div className="panelIzquierda">
+            <a>
+                <h3>Categorias</h3>
+            </a>
+            <a>
+                <h3>Crear post</h3>
+            </a>
+        </div>
+        <div className="posts">
+            <script>//meterlo en un .map por cada post que exista</script>
+            <div className="post">
+                <div className="titulo">
+                    <h3>Titulo</h3>
+                </div>
+                <div className="Categoria">
+                    <p>Categoria</p>
+                </div>
+                <div className="botones">
+                    <button>Ver post</button>
+                    <button>Editar post</button>
+                </div>
+                <div className="fecha">
+                    <p>Fecha y hora</p>
+                </div>
+            </div>
+        </div>
+        <div className="footer">
+            <h4>Footer</h4>
+        </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
  *  
 */
 Route::get('/', function () {
-    return view('welcome');    //Cambiar dirección a home y usar controlador HomeController
+    return view('home');    //Cambiar dirección a home y usar controlador HomeController
 });
 
 Route::get('/login', function () {
