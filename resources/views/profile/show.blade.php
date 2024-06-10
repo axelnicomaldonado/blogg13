ff2c8295d882cad08485a62e809ebcb3d20cba2c
thomasrifo <thomasmrifo@gmail.com>
2024-06-10 02:56:11 -0300

    perfil

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -33,43 +33,60 @@
                 </div>
             </div>
 
-            <div className="posts">
-                @if ($posts->count() > 0)
-                <div class="flex items-center justify-center mt-16">
-                    <h3 class="text-3xl font-bold text-white">Publicaciones</h3>
-                </div>
+            <div class="pb-8">
                 @foreach ($posts as $post)
-                    <div class="mt-10 py-8 max-w-7xl mx-auto sm:px-6 lg:px-8 bg-white dark:bg-gray-800 
-                    overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900 dark:text-gray-100">
-                        <div class="text-3xl font-bold">
-                            <h3>{{$post->title}}</h3>
-                        </div>
-                                <div class="text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800">
-                                    <p>{{$user->username}}</p>
-                                </div>
-                        <div className="botones">
-                            <a href="/category/show/{{$post->id}}" class="font-bold inline-flex text-blue-400 
-                            px-4 py-2">
-                                <p>Ver post</p>
-                            </a>
-                            @if(Auth::check() && Auth::user()->id == $post->user_id)
-                                <a href="/category/edit/{{$post->id}}" class="font-bold inline-flex text-blue-400 
-                                px-4 py-2">
-                                    <p>Editar post</p>
+                    <div
+                        class="mt-10 pt-8 max-w-4xl mx-auto sm:px-6 lg:px-8 bg-white dark:bg-gray-800 
+                    overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900 dark:text-gray-100 h-80">
+        
+        
+                        <div class="flex">
+                            <div class="container w-xl h-4xl">
+                                <a href="/category/show/{{ $post->id }}">
+                                    <img src="{{ asset('storage/imagenes/posts/' . $post->poster) }}"
+                                        class="pr-4 bg-cover hover:scale-105">
                                 </a>
-                            @endif
+                                    <div class="flex">
+                                        <div class="text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 container py-1">
+                                            <a href="/profile/{{ $user->username }}">
+                                                <p class="hover:text-blue-500">{{ $user->username }}</p>
+                                            </a>
+                                        </div>
+                                        <div class="justify-end container">
+                                            @if (Auth::check() && Auth::user()->id == $post->user_id)
+                                                <a href="/category/edit/{{ $post->id }}"
+                                                    class="font-bold inline-flex text-blue-400 px-4 py-1">
+                                                    <p>Editar post</p>
+                                                </a>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                            
+                                    <div class="text-gray-500">
+                                        <p>{{ $post->created_at }}</p>
+                                    </div>
+        
+        
+                            </div>
+        
+                            <div class="justify-start max-w-xl container">
+                                <div class="text-3xl font-bold mb-2">
+                                    <a href="/category/show/{{ $post->id }}" class="hover:text-blue-500">
+                                        <h3>{{ $post->title }}</h3>
+                                    </a>
+                                </div>
+                                <div class="text-4 font-bold line-clamp-6">
+                                    <p>{!! $post->content !!} </p>
+                                </div>
+                                <a href="/category/show/{{ $post->id }}" class="text-blue-500">ver mas</a>
+                            </div>
                         </div>
-                        <div class="text-gray-500">
-                            <p>{{$post->created_at}}</p>
-                        </div>
+        
+        
                     </div>
                 @endforeach
-                @else
-                    <div class="flex items-center justify-center h-64 mt-16">
-                        <h3 class="text-3xl font-bold text-white">No hay publicaciones</h3>
-                    </div>
-            @endif
-        </div>
+            </div>
     </div>
     </div>
     </div>
